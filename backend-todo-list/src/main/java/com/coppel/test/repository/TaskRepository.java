a2744452ad8f38403e709637c705cb013341fd12
DAVID ADRIAN GARCIA CASTAÑEDA <88115478+dgarcia92@users.noreply.github.com>
2023-02-19 13:22:09 -0600

    Primer Commit - Coppel Test

--- backend-todo-list/src/main/java/com/coppel/test/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coppel.test.repository;
-
-import com.coppel.test.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
